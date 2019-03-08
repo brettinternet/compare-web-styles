31dc493aa08613fd3f39da7a83003a793c248260
brettinternet <brettinternet@gmail.com>
2019-03-08 12:42:07 -0700

    add syntax highlighting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
 import Layout from "../components/layout"
 import styled from "styled-components"
-import beautifyCss from 'utils/beautifyCss'
+import beautifyCss from "utils/beautifyCss"
+import Prism from "prismjs"
+import "utils/prismStyles.css"
 
 import {
   BootstrapButton,
@@ -63,59 +65,78 @@ const buttons = [
   },
 ]
 
-const IndexPage = ({ location }) => (
-  <Layout
-    headProps={{
-      title: "Home",
-      keywords: ["styles", "application", "components", "library"],
-    }}
-    location={location}
-  >
-    <div className="container mt-4">
-      <h1>Buttons</h1>
-      <table className="table table-responsive-sm">
-        <thead>
-          <tr>
-            <th scope="col">Source</th>
-            <th scope="col">Type</th>
-            <th scope="col">License</th>
-            <th scope="col" className="text-center">
-              Button
-            </th>
-            <th scope="col">Styles</th>
-          </tr>
-        </thead>
-        <tbody>
-          {buttons.map(
-            ({ name, type, license, component: Component, styles }, index) => {
-              const styleString =
-                styles && beautifyCss(styles[0])
-              return (
-                <tr key={index}>
-                  <TH scope="row">{name}</TH>
-                  <TD className="text-muted">{type}</TD>
-                  <TD className="text-muted">{license}</TD>
-                  <TD className="text-center">
-                    <Component />
-                  </TD>
-                  <TD>
-                    <Pre>
-                      <code>{styleString}</code>
-                    </Pre>
-                  </TD>
-                </tr>
-              )
-            }
-          )}
-        </tbody>
-      </table>
-    </div>
-  </Layout>
-)
+class IndexPage extends React.Component {
+  componentDidMount() {
+    Prism.highlightAll()
+  }
+
+  render() {
+    const { location } = this.props
+
+    return (
+      <Layout
+        headProps={{
+          title: "Home",
+          keywords: ["styles", "application", "components", "library"],
+        }}
+        location={location}
+      >
+        <div className="container mt-4">
+          <h1>Buttons</h1>
+          <table className="table table-responsive-sm">
+            <thead>
+              <tr>
+                <th scope="col">Source</th>
+                <th scope="col">Type</th>
+                <th scope="col">License</th>
+                <th scope="col" className="text-center">
+                  Button
+                </th>
+                <th scope="col">Styles</th>
+              </tr>
+            </thead>
+            <tbody>
+              {buttons.map(
+                (
+                  { name, type, license, component: Component, styles },
+                  index
+                ) => {
+                  const styleString = styles && beautifyCss(styles[0])
+                  return (
+                    <tr key={index}>
+                      <TH scope="row">{name}</TH>
+                      <TD className="text-muted">{type}</TD>
+                      <TD className="text-muted">{license}</TD>
+                      <TD className="text-center">
+                        <Component />
+                      </TD>
+                      <TD>
+                        <Pre>
+                          <code className="language-css">{styleString}</code>
+                        </Pre>
+                      </TD>
+                    </tr>
+                  )
+                }
+              )}
+            </tbody>
+          </table>
+        </div>
+      </Layout>
+    )
+  }
+}
 
 const Pre = styled.pre`
-  word-wrap: break-word;
-  white-space: pre-wrap;
+  &&& {
+    border: none;
+    font-size: 12px;
+
+    > code {
+      word-wrap: break-word;
+      white-space: pre-wrap;
+    }
+  }
 `
 
 const TH = styled.th`
